c914a2ed5da1e392e2413072d03256043d371857
Carston Hernke <hernk009@umn.edu>
2018-11-03 18:58:32 -0500

    fixed file references

--- js/portrait.js
+++ js/portrait.js
@@ -1,5 +1,5 @@
 // get image using vanilla js
-var img = document.getElementById("portrait_image")
+var img = document.querySelector('.dot-tone-portrait img');
 
 // init halftone
 new BreathingHalftone( img, {
